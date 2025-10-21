60cd990c65ba61317ae01422906bdfb0ff175773
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-10-21 23:37:49 +0300

    Changed main.py, category.py, product.py

--- src/category.py
+++ src/category.py
@@ -1,15 +1,28 @@
+from src.product import Product
+
+
 class Category:
     """Класс для представления категории товаров в интернет-магазине."""
 
     name: str
     description: str
-    products: list[str]
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def products(self):
+        product_str = ''
+        for product in self.__products:
+            product_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
--- src/product.py
+++ src/product.py
@@ -1,13 +1,32 @@
+from typing import Any
+
+
 class Product:
     """Класс для представления товара в интернет-магазине."""
 
     name: str
     description: str
-    price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product: dict[str, Any]):
+        result = cls(product['name'], product['description'], product['price'], product['quantity'])
+        return result
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+            return
+
+        self.__price = new_price
