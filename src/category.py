624e62be65a25dbedb0b3c45d784bd769ec98482
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-10-23 22:19:30 +0300

    Modified category.py, product.py, main.py Created lawn_grass.py, smartphone.py

--- src/category.py
+++ src/category.py
@@ -29,8 +29,11 @@ class Category:
     def add_product(self, product: Product) -> None:
         """Метод добавляет товар в категорию"""
 
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products_in_list(self):
---
+++ src/lawn_grass.py
@@ -0,0 +1,40 @@
+from src.product import Product
+
+class LawnGrass(Product):
+    """Класс для представления товара 'Lawn Grass' в интернет-магазине."""
+
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+
+    def __add__(self, other):
+        if type(other) is LawnGrass:
+            return self.price * self.quantity + other.price * other.quantity
+        else:
+            raise TypeError
+
+
+if __name__ == "__main__":
+    lawn_grass = LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый")
+
+    print(lawn_grass.name)
+    print(lawn_grass.description)
+    print(lawn_grass.price)
+    print(lawn_grass.quantity)
+    print(lawn_grass.country)
+    print(lawn_grass.germination_period)
+    print(lawn_grass.color)
