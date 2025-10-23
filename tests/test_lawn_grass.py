87fdbfe08bea4d93c115eb953ef201a8cf603bc7
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-10-23 22:44:08 +0300

    Modified conftest.py, main.py Created tests for lawn_grass.py, smartphone.py

---
+++ tests/test_lawn_grass.py
@@ -0,0 +1,25 @@
+import pytest
+
+def test_lawn_grass_init(lawn_grass1):
+    """Тест на проверку корректности инициализации класса LawnGrass"""
+
+    assert lawn_grass1.name == 'Газонная трава'
+    assert lawn_grass1.description == 'Элитная трава для газона'
+    assert lawn_grass1.price == 500.0
+    assert lawn_grass1.quantity == 20
+    assert lawn_grass1.country == 'Россия'
+    assert lawn_grass1.germination_period == '7 дней'
+    assert lawn_grass1.color == 'Зеленый'
+
+
+def test_lawn_grass_add(lawn_grass1, lawn_grass2):
+    """Тест проверки сложения двух продуктов"""
+
+    assert lawn_grass1 + lawn_grass2 == 500.0 * 20 + 450.0 * 15
+
+
+def test_lawn_grass_add_error(lawn_grass1):
+    """Тест проверки на ошибку если слагаемое не относится к классу LawnGrass"""
+
+    with pytest.raises(TypeError):
+        result = lawn_grass1 + 1
---
+++ tests/test_smartphone.py
@@ -0,0 +1,26 @@
+import pytest
+
+def test_smartphone_init(smartphone1):
+    """Тест на проверку корректности инициализации класса Smartphone"""
+
+    assert smartphone1.name == 'Iphone 15'
+    assert smartphone1.description == '512GB, Gray space'
+    assert smartphone1.price == 210000.0
+    assert smartphone1.quantity == 8
+    assert smartphone1.efficiency == 98.2
+    assert smartphone1.model == '15'
+    assert smartphone1.memory == 512
+    assert smartphone1.color == 'Gray space'
+
+
+def test_smartphone_add(smartphone1, smartphone2):
+    """Тест проверки сложения двух продуктов"""
+
+    assert smartphone1 + smartphone2 == 210000.0 * 8 + 31000.0 * 14
+
+
+def test_smartphone_add_error(smartphone1):
+    """Тест проверки на ошибку если слагаемое не относится к классу Smarphone"""
+
+    with pytest.raises(TypeError):
+        result = smartphone1 + 1
